37019a2a9d6f3574d9dac08b47e0d9c978aa56bb
nrwatson16 <nrwatson@pm.me>
2025-02-02 10:10:27 -0600

    Update trainingcal.py

--- trainingcal.py
+++ trainingcal.py
@@ -11,7 +11,7 @@ from calendar import monthcalendar, month_name
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def get_base_url():
-    return "https://traincal.streamlit.app"
+    return "https://trainingcal.streamlit.app"
 
 # Load environment variables
 load_dotenv()
